f05ac4798d951edf84f368b77b1219ee4e8f8e2a
fj7708 <110383912+fj7708@users.noreply.github.com>
2022-11-07 15:38:58 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,29 +1,42 @@
 class BinaryTreeNode:
     def __init__(self, data):
         self.data = data
-        self.left_child = None
-        self.right_child = None
+        self.left = None
+        self.right = None
 
 
 def insert(root, new_value) -> BinaryTreeNode:
-    """If binary search tree is empty, make a new node, declare it as root and return the root.
-        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
-        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
-        Finally, return the root.
-        """
+     return the root.
+        
     # Write your code here
+    if root == None:
+        root = BinaryTreeNode(new_value)
+    elif new_value < root.data:
+        root.left = insert(root.left, new_value)
+    else: #new_value >= root.data:
+        root.right = insert(root.right, new_value)
+    return root        
 
 
 def inorder(root) -> None:
-    # Write your code here
+    if root: 
+        inorder(root.left)
+        print(root.data, end = " ")
+        inorder(root.right)
 
 
 def preorder(root) -> None:
-    # Write your code here
+    if root:
+        print(root.data, end = " ")
+        preorder(root.left)
+        preorder(root.right)
 
 
 def postorder(root) -> None:
-    # Write your code here
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data, end = " ")
 
 
 # Do not change the following code
